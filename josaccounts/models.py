20dcbd0ac9d44d50b11fe821c31f7f1f5a7cf0c7
addsimm <adamfsimon@gmail.com>
2016-01-26 16:35:57 -0800

    Fixed about mission - next: finish user profile admin and templates

--- josaccounts/models.py
+++ josaccounts/models.py
@@ -6,7 +6,14 @@ from django.contrib.auth.models import User
 
 
 class JOSProfile(models.Model):
+    class Meta:
+        verbose_name = 'JOS Member Profile'
+        verbose_name_plural = 'JOS Members Profile'
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(null=True, blank=True)
+    join_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+
     background = models.TextField(null=True, blank=True)
 
