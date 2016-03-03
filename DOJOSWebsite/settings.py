35b1a46d9fa9956bad159256a922e08b78dc6d47
addsimm <adamfsimon@gmail.com>
2016-03-02 16:16:58 -0800

    changed password reset message

--- DOJOSWebsite/settings.py
+++ DOJOSWebsite/settings.py
@@ -296,11 +296,12 @@ OPTIONAL_APPS = (
 #########
 
 # EMAIL_SUBJECT_PREFIX = 'Join Our Story'
-# DEFAULT_FROM_EMAIL = SERVER_EMAIL = EMAIL_HOST_USER = 'joinus@joinourstory.com'
+# SERVER_EMAIL = EMAIL_HOST_USER = 'joinus@joinourstory.com'
 # EMAIL_USE_TLS = True
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_HOST_PASSWORD = '4Primetime!'
 # EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = "joinus@joinourstory.com"
 
 ##################
 # LOCAL SETTINGS #
