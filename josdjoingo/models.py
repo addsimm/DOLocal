19a20a60020c81a94d1f6b041ccf4ee9096ff5d2
addsimm <adamfsimon@gmail.com>
2016-03-06 10:20:12 -0800

    cloud upload widget not working - somethign wrong with jquery
    begin joingo - added app

--- josdjoingo/models.py
+++ josdjoingo/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+
+
+from mezzanine.core.models import TimeStamped
 
 # Create your models here.
+
+def pick_game_number():
+    game_number = random.randrange(1, 75, 1)
+    return game_number
+
+
+class JoingoCard(TimeStamped, models.Model):
+    class Meta:
+        verbose_name = 'Joingo Card'
+        verbose_name_plural = 'Joingo Cards'
+
+    entries = ArrayField(models.PositiveSmallIntegerField())
+
+    # card_owner_team = models.ForeignKey(JoingoTeam)
+
+
+
+class JoingoTeam():
+    team_name = models.CharField(max_length=30)
+    # team_players
+
+
+class JoingoGame():
+    pass
+
+
+class JoingoPlayer():
+    player_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # player_teams = models.ManyToManyField(JoingoTeam)
--- josdjoingo/views.py
+++ josdjoingo/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 
+import random
+
+from .models import JoingoCard
+
 # Create your views here.
+
+def generate_card_entries():
+    entries = []
+    for letter in 'BINGO':
+        for row in range(1, 60, 15):
+            column = list(range(15))
+            random.shuffle(column)
+            column = column[-5:]
+            column = column * row
+            entries.append(column)
+
+    return entries
 
 
 def djoingo_main(request):
@@ -9,7 +25,10 @@ def djoingo_main(request):
     Main Djoingo View
     """
     template = "josdjoingo/djoingo_main.html"
-    context = {}
+    card = JoingoCard()
+    card.entries = generate_card_entries()
+    card.save()
+    context = {"card": card}
 
     return TemplateResponse(request, template, context)
 
