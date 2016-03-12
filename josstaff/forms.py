c1ab44e3d4e00ac0f6fc775275af51802db2340c
addsimm <adamfsimon@gmail.com>
2016-03-12 14:37:25 -0800

    cloud upload widget opening but nto saving
    timesheet - date picker works need to bind instance and gather data for post/save
    
    begin joingo - 5 min story

--- josstaff/forms.py
+++ josstaff/forms.py
@@ -14,11 +14,12 @@ class JOSStaffHoursEntryForm(Html5Mixin, forms.ModelForm):
         model = JOSStaffHoursEntry
         fields = '__all__'
         widgets = {
-            'period_date_start': forms.DateInput(),
+            'period_date_start': forms.TextInput(attrs={'type': 'date'}),
             'period_date_end': forms.DateInput()
         }
 
     member = forms.CharField(label="Member", widget=forms.TextInput(), initial='z')
+    date_field = forms.DateField(widget=forms.TextInput(attrs={'type': 'date'}))
 
     def __init__(self, *args, **kwargs):
         super(JOSStaffHoursEntryForm, self).__init__(*args, **kwargs)
