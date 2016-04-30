b91d09672b80d96b10cc216fe2102de4eed13186
addsimm <adamfsimon@gmail.com>
2016-04-30 16:31:02 -0700

    still setting up messaging: next address - username change to josname

--- DOJOSWebsite/settings.py
+++ DOJOSWebsite/settings.py
@@ -12,6 +12,8 @@ import os
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.241.204.118', '*']
 
+DATETIME_FORMAT = "D, M d - fA"
+
 # Local time zone. Choice codes: wikipedia
 TIME_ZONE = 'America/Los_Angeles'
 
