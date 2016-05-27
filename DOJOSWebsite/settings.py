717b92a7fd7e12ca3072c21fbfd627dfdfc9cc42
addsimm <adamfsimon@gmail.com>
2016-05-27 10:01:14 -0700

    css refactor - breadcrumb refactor - next restart pybb

--- DOJOSWebsite/settings.py
+++ DOJOSWebsite/settings.py
@@ -12,7 +12,9 @@ import os
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.241.204.118', '*']
 
-DATETIME_FORMAT = "D, M d - f A"
+DATEFORMAT = "M d"
+TIMEFORMAT = "D, f A"
+DATETIME_FORMAT = "D, M d f A"
 
 # Local time zone. Choice codes: wikipedia
 TIME_ZONE = 'America/Los_Angeles'
