d097f34150155f3d055a62010a8c75100b39a703
addsimm <adamfsimon@gmail.com>
2016-10-10 22:16:53 -0700

    handouts display working

--- joscourses/views.py
+++ joscourses/views.py
@@ -20,35 +20,43 @@ def course_week_list(request, template="###", extra_context=None):
 def course_week(request, week_no="0", part_no="9", segment_no="9", handout_id="1", template="joscourses/joshandout.html", extra_context=None):
 
     week = get_object_or_404(JOSCourseWeek, week_no=1)
-    handouts = JOSHandout.objects.filter(courseweek=week, part_no = part_no, publish=True).order_by('segment_no', 'element_order')
-    segments = handouts.distinct().order_by('segment_no')
-
-### need handouts for segment
-
+    week_handouts = JOSHandout.objects.filter(courseweek=week, part_no = int(part_no), publish=True)
+    week_segments = week_handouts.distinct('segment_no').order_by('segment_no')
 
     try:
-        segment_no = segments[0].segment_no
+        first_segment_no = week_segments[0].segment_no
     except:
-        segment_no = 9
+        first_segment_no = 9
 
-    try:
-        handout = handouts[0]
-    except:
-        handout = get_object_or_404(JOSHandout, pk=1)
+    if segment_no != "9":
+        current_segment_no = int(segment_no)
+    else:
+        current_segment_no = first_segment_no
 
-    if not handout.pdf_handout:
-        handout.pdf_handout = 'missing'
+    current_handouts = week_handouts.filter(segment_no=current_segment_no).order_by('element_order')
 
-    current_segment_no = handout.segment_no
-    current_segment = get_object_or_404(segments, segment_no=current_segment_no)
+    if handout_id == '1':
+        try:
+            cur_handout = current_handouts[0]
+        except:
+            cur_handout = get_object_or_404(JOSHandout, pk=1)
+    else:
+        try:
+            cur_handout = get_object_or_404(JOSHandout, pk=int(handout_id))
+        except:
+            cur_handout = get_object_or_404(JOSHandout, pk=1)
+
+    pdf_missing = False
+    if not cur_handout.pdf_handout:
+        pdf_missing = True
 
     context = { 'week':       week,
-                'segment_no': segment_no,
-                'handout_id': handout_id,
-                'segments':   segments,
-                'current_segment': current_segment,
-                'handouts':   handouts,
-                'handout':    handout
+                'part_no': int(part_no),
+                'segments':   week_segments,
+                'current_segment': current_segment_no,
+                'handouts':   current_handouts,
+                'handout': cur_handout,
+                'pdf_missing': pdf_missing
                }
 
     context.update(extra_context or {})
