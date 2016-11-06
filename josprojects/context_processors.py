d2e4c6f47d4140188bded04aa9e1c75a0630ec69
addsimm <adamfsimon@gmail.com>
2016-11-05 21:04:36 -0700

    my notes functioning!

--- josprojects/context_processors.py
+++ josprojects/context_processors.py
@@ -1,30 +1,30 @@
-from django.shortcuts import render, get_object_or_404
 
+from numbers import Number
+from django.contrib.auth import get_user_model
 
 from josmembers.models import JOSUserCreatedNote, JOSProfile
-from django.contrib.auth import get_user_model
+from .models import JOSHelpItem
 
 User = get_user_model()
 
-from .models import JOSHelpItem
-
-### CHANGE TO SUIT HELP SYSTEM
-
 def help_sys(request):
-    help_items = JOSHelpItem.objects.all
 
-    user_id = request.user.id
+    context = {}
+    if isinstance(request.user.id, Number):
+        help_items = JOSHelpItem.objects.all
 
-    user = User.objects.get(id = user_id)
+        user = User.objects.get(id =request.user.id)
 
 
-    profile = JOSProfile.objects.get(user=user)
+        profile = JOSProfile.objects.get(user=user)
 
-    user_created_note, created = JOSUserCreatedNote.objects.get_or_create(profile = profile)
+        user_created_note, created = JOSUserCreatedNote.objects.get_or_create(profile = profile)
 
-    text = user_created_note.note_text
+        text = user_created_note.note_text
 
-    return {
-        'help_items': help_items,
-        'user_created_note': text
-    }
+        context = {
+            'help_items': help_items,
+            'user_created_note': text
+        }
+
+    return context
