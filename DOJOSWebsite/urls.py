39e8f41d60f673624bdefde3cf5276654d51ba88
addsimm <adamfsimon@gmail.com>
2016-11-10 07:59:23 -0800

    handout display, print, download working

--- DOJOSWebsite/urls.py
+++ DOJOSWebsite/urls.py
@@ -7,7 +7,6 @@ from django.contrib import admin
 from mezzanine.conf import settings
 
 from josprojects.views import *
-from joscourses.views import course_week_test
 
 _slash = "/" if settings.APPEND_SLASH else ""
 _verify_pattern = "/(?P<uidb36>[-\w]+)/(?P<token>[-\w]+)"
@@ -32,11 +31,9 @@ urlpatterns += patterns('',
 
     ##### Adam test page: currently set for courseweek
 
-    url("test_page/(?P<week_no>\d{1,3})/(?P<part_no>\d{1,3})/(?P<segment_no>\d{1,3})/(?P<handout_id>\d{1,4})/$",
-    course_week_test,
-    name="course_week_test"),
-
-    url("course_week_test$", course_week_test, name="course_week_test"),
+    # url("test_page/(?P<week_no>\d{1,3})/(?P<part_no>\d{1,3})/(?P<segment_no>\d{1,3})/(?P<handout_id>\d{1,4})/$", course_week_test, name="course_week_test"),
+    #
+    # url("course_week_test$", course_week_test, name="course_week_test"),
 
 
     ### EDITABLE HOMEPAGE
